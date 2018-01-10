deda9ba6143da290cc8f6c99452d2f5cb0f4c90a
Owner <owner@Owners-MacBook-Pro.local>
2018-01-10 14:27:22 -0800

    simple js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.body.style.background="red";
+document.body.style.font="sans-serif";
+document.getElementById("color").style.backgroundColor= "blue";
+document.getElementById("nickname").innerHTML = "Marusya";
+document.getElementById("favorites").innerHTML = "Coding, traveling, food, books, and food again.";
+document.getElementById("hometown").innerHTML = "San Francisco";
